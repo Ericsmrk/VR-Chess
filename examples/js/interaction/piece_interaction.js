68d0e7cf1a05075c72efbb5adbeb10bb0fa8319b
millerjacob49 <miller_jacob49@outlook.com>
2022-09-29 19:44:17 -0700

    separating javascript. Still testing cursor

---
+++ examples/js/interaction/piece_interaction.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent('cursor-listener', {
+			  init: function () {
+				var lastIndex = -1;
+				var COLORS = ['red', 'green', 'blue'];
+
+				this.el.addEventListener('mouseenter', function(evt){
+					this.setAttribute('material', 'opacity', 0.5);
+				})
+				this.el.addEventListener('mouseleave', function(evt){
+					this.setAttribute('material', 'opacity', 1);
+				})
+				this.el.addEventListener('click', function (evt) {
+				  lastIndex = (lastIndex + 1) % COLORS.length;
+				  this.setAttribute('material', 'color', COLORS[lastIndex]);
+				  console.log('I was clicked at: ', evt.detail.intersection.point);
+
+				  
+				});
+
+			  }
+			});
