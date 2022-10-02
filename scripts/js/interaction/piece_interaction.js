3efc5d64c76687dfad94730bc10d2173277e6a23
millerjacob49 <miller_jacob49@outlook.com>
2022-10-01 19:55:56 -0700

    instlld component, working on movement

--- scripts/js/interaction/piece_interaction.js
+++ scripts/js/interaction/piece_interaction.js
@@ -2,7 +2,9 @@ AFRAME.registerComponent('cursor-listener', {
 			  init: function () {
 				var lastIndex = -1;
 				var COLORS = ['red', 'green', 'blue'];
-				var POSITIONS = [1, 2, 3]
+				var POSITIONz = -2;
+				var piece_id = "";
+				var move_id = "";
 
 				this.el.addEventListener('mouseenter', function(evt){
 					this.setAttribute('material', 'opacity', 0.5);
@@ -11,13 +13,21 @@ AFRAME.registerComponent('cursor-listener', {
 					this.setAttribute('material', 'opacity', 1);
 				})
 				this.el.addEventListener('click', function (evt) {
-				  lastIndex = (lastIndex + 1) % COLORS.length;
-				  this.setAttribute('material', 'color', COLORS[lastIndex]);
-				  console.log('I was clicked at: ', evt.detail.intersection.point);
-				  this.setAttribute('position', {x:0, y:POSITIONS[lastIndex], z:-3})
-
-				  
-				});
-
+				 if(piece_id != ""){
+					move_id = this.id;
+					alert(move_id);
+					if(piece_id.charAt(0) == move_id.charAt(0)){
+						
+					}
+				 }
+				 else{
+				 var pos = window.prompt("Enter square to move to");		//Hopefully don't need this.
+				  piece_id = this.id;
+				  alert(piece_id);
+				 
+				}
+			    })
+				
+				
 			  }
-			});
+		});
