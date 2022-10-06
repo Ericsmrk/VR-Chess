263290c22dec0dda8cd66beac42a56e851d3a44f
millerjacob49 <miller_jacob49@outlook.com>
2022-10-06 13:41:19 -0700

    Wokring on movement

--- scripts/js/interaction/piece_interaction.js
+++ scripts/js/interaction/piece_interaction.js
@@ -1,8 +1,20 @@
+
 AFRAME.registerComponent('cursor-listener', {
 			  init: function () {
+				//schema: {}, //THIS STORES DATA!!
 				var lastIndex = -1;
 				var COLORS = ['red', 'green', 'blue'];
 				var POSITIONS = [1, 2, 3]
+				var posx = 0;
+				var posy = 0;
+				var objID = "NULL";
+				var beforePos = "0";
+/*
+				function objectToPos(posObject){
+					return posObject.x + " " + posObject.y + " " + posObject.z;
+				}*/
+
+				
 
 				this.el.addEventListener('mouseenter', function(evt){
 					this.setAttribute('material', 'opacity', 0.5);
@@ -10,14 +22,45 @@ AFRAME.registerComponent('cursor-listener', {
 				this.el.addEventListener('mouseleave', function(evt){
 					this.setAttribute('material', 'opacity', 1);
 				})
-				this.el.addEventListener('click', function (evt) {
-				  lastIndex = (lastIndex + 1) % COLORS.length;
-				  this.setAttribute('material', 'color', COLORS[lastIndex]);
-				  console.log('I was clicked at: ', evt.detail.intersection.point);
-				  this.setAttribute('position', {x:0, y:POSITIONS[lastIndex], z:-3})
+				rook_w_r.addEventListener('mousedown', function (obj) {
+					
+					
+					
+					
+				  console.log("Hello?"); //only triggers console.log once<----not true, infinitely loops 'alert'
+				 
+				  
+				  this.addEventListener('mouseup', function(evt) {
+					console.log("Hi");
+					objID = this.id;
+					console.log(objID);
+					if(objID == "rook_w_r"){
+					
+					/*
+					var pos = this.getAttribute('position');
+					pos = objectToPos(pos);
+					console.log(beforePos);
+					rook_w_r.setAttribute('animation', `property: position; from ${beforePos} to ${targetPos}; dur:`);
+					*/
+					rook_w_r.setAttribute('position', {x:2, y:2, z:-2});}
+				  })
+				 
+				  				
 
 				  
-				});
+				},)
+				
 
+				
+
+			  },/*
+			  update: function() {
+					this.el.addEventListener('click', function(evt){
+						alert("What happened?" + this.id);
+						posx = getAttribute(this.position.x);
+						alert(posx);
+
+					})
 			  }
+			*/
 			});
