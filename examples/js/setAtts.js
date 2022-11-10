bca792321ce09ad34885c7de09b9c48508f186f7
BrettH23 <brettonh2@gmail.com>
2022-11-10 13:42:18 -0800

    Moved position of everything to center. Also changed some equations to be more intuitive.

--- examples/js/setAtts.js
+++ examples/js/setAtts.js
@@ -3,11 +3,12 @@ AFRAME.registerComponent('setatts',{
         const pieces = document.querySelectorAll('.chessguy');
         var chessPieceScale = 0.0355489829436;  //ratio of 'size' of model to smaller size, based on 15cm tall king (larger than usual piece)
         for(i in pieces){
+            pieces[i].object3D.position.x -=0.25;
             pieces[i].object3D.position.x *=0.16;
-            pieces[i].object3D.position.x +=0.25*(1-0.16);
             
+            
+            pieces[i].object3D.position.z -=0.25;
             pieces[i].object3D.position.z *=0.16;
-            pieces[i].object3D.position.z +=-2.75*(1-0.16);
             
             pieces[i].object3D.position.y =0.8;
             pieces[i].object3D.scale.set(chessPieceScale,chessPieceScale,chessPieceScale);
