aaddc10d4736cdb06a5c7375c617faca9d6513c5
BrettH23 <113479725+BrettH23@users.noreply.github.com>
2022-11-15 14:18:27 -0800

    Deterministic color seeds added. Paving the way for color data transfer between scenes.

--- examples/js/color_changer_component.js
+++ examples/js/color_changer_component.js
@@ -4,40 +4,90 @@ AFRAME.registerComponent('color-changer',{
         seed:{type:'number',default:-1}
     },
     init:function(){
+        
         const pieces = document.querySelectorAll('.chessguy');
+        console.log(pieces);
+
         var currentSeed;
-        if(this.data.seed < 0){
-            currentSeed = Math.floor(Math.random()*0xFFFF);
-        }else{
-            currentSeed = this.data.seed;
-            currentSeed = currentSeed % 0xFFFF;
+        function rerollSeed(){
+            currentSeed = Math.floor(Math.random()*0x10000);
+            let appendGuy = document.createElement('p');
+            appendGuy.innerHTML = currentSeed;
+            document.querySelector('.inputBox').appendChild(appendGuy);
+            
+            //this.setAttribute('seed', currentSeed);
         }
+        function advanceSeed(){
+            currentSeed = (currentSeed*34819)% 0xFFFF;
+        }
+        //Check if there is a seed, if not, make one. otherwise set seed to max size.
+        
+        
         
         
         //console.log(pieces)
         function getRandomColor() {
-            let r = (Math.floor(Math.random()*256)).toString(16);
-            let g = (Math.floor(Math.random()*256)).toString(16);
-            let b = (Math.floor(Math.random()*256)).toString(16);
-            if(r.length == 1){
-                r = '0'+r;
-            }if(g.length == 1){
-                g = '0'+g;
-            }if(b.length == 1){
-                b = '0'+b;
+            //console.log((currentSeed).toString(16));
+            let rgb = (Math.floor(currentSeed/16)).toString(16);//bottom bit is not used for better pseudo randomization
+            //rgb = '0';
+            //console.log(rgb + 'random test');
+            if(rgb.length<3){
+                if(rgb.length<2){
+                    rgb = '0'+rgb;
+                }
+                rgb = '0'+rgb;
             }
-            return '#'+r+b+g;
+            //console.log(rgb + 'random test');
+            advanceSeed();
+            return '#'+rgb;
             
         }
         function setColors(){
-            console.log('checkrunning');
             for (let i in pieces){
+                
+                color = getRandomColor();
+                console.log(color);
                 let pieceToRecolor = pieces[i];
                 pieceToRecolor.removeAttribute('piece-color');
-                pieceToRecolor.setAttribute('piece-color',getRandomColor());
+                pieceToRecolor.setAttribute('piece-color',color);
+            
+                
             }
         }
-        this.el.addEventListener('click', setColors);
+
+        this.el.addEventListener('click', function(){
+            rerollSeed();
+            //console.log(this.data.seed);
+            setColors();
+            
+        });
+        
+        let originalSeed = this.data.seed;
+        
+            if(originalSeed < 0){
+                rerollSeed();
+            }else{
+                currentSeed = originalSeed;
+                currentSeed = currentSeed % 0xFFFF;
+                var promises = Array(32);
+                for(let i =0;i<32;i++){
+                    console.log('checkme')
+                    promises[i] = new Promise(function(resolve) {
+                        pieces[i].addEventListener('model-loaded',resolve,false);
+                        
+                    });
+                }
+                Promise.all(promises).then(function(){
+                    setColors();
+                    
+                });
+                
+                
+            
+   
+            }
+        
+        
         
     }
 });
