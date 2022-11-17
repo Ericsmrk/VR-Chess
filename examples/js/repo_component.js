fb8df282c3d7a3a9146c719853564bb38fcb7ea3
millerjacob49 <miller_jacob49@outlook.com>
2022-11-16 18:26:05 -0800

    Mobile bug fix.

--- examples/js/repo_component.js
+++ examples/js/repo_component.js
@@ -2,27 +2,32 @@ AFRAME.registerComponent('repo_b', {
 
 init: function() {
     const avatar1 = document.querySelector('#player')
+    const cam = document.querySelector('cameraRig')
     const object3D = this.el.object3D; //is this necessary? Still dont understand it
 
     
     function setPos(){
-        console.log(avatar1)
-        console.log("yes")
-        const whitePos = new THREE.Vector3(0,0,0)
+
         //avatar1.object3D.position.copy(whitePos)
+        avatar1.removeAttribute('look-controls')
+        
         avatar1.setAttribute('position', "0 1.2 -0.4")
-        avatar1.setAttribute('rotation', "180 90 90")
+        //avatar1.setAttribute('rotation', {x:0, y:180, z:0})
+        avatar1.object3D.rotation.set(THREE.Math.degToRad(0),THREE.Math.degToRad(180),THREE.Math.degToRad(0))
+        avatar1.setAttribute('look-controls', "pointerLockEnabled:true")
 
         console.log("yup")
     }
 
     this.el.addEventListener('click', setPos);
-}
 
+    
+}
 
 
 });
 
+/*
 AFRAME.registerComponent('repo_w', {
 
     init: function() {
@@ -46,3 +51,4 @@ AFRAME.registerComponent('repo_w', {
     
     
     });
+*/
