ac7974f1c414b2069686d84a4896bea01ab9c5ab
BrettH23 <113479725+BrettH23@users.noreply.github.com>
2022-11-18 13:51:21 -0800

    Data transfer between scenes done Selected scene can be switched in main menu. Laggy, unfort. Colors, room code, and selected scene can all be selected from the menu.

--- examples/js/setAtts.js
+++ examples/js/setAtts.js
@@ -2,6 +2,45 @@ AFRAME.registerComponent('setatts',{
     init:function(){
         const pieces = document.querySelectorAll('.chessguy');
         var chessPieceScale = 0.0355489829436;  //ratio of 'size' of model to smaller size, based on 15cm tall king (larger than usual piece)
+        var params = this.getUrlParams();
+        var colorChanger= document.querySelector('#initial-color-changer');
+        var loader = document.querySelector('#loader');
+        let colorAtt = '';
+        if(colorChanger){
+            if(params.colorType){
+                colorAtt+='option:' + params.colorType + ';';
+                
+                if(params.colorType=='gradient'){
+                    if(params.gradW){
+                        colorAtt+='gradientW:' + params.gradW + ';'
+                    }if(params.gradB){
+                        colorAtt+='gradientB:' + params.gradB + ';'
+                    }    
+                }
+            }
+            if(params.colorSeed){
+                colorAtt+='seed:' + params.colorSeed + ';';
+            }
+            
+           
+            
+            colorAtt+='onload:true'
+            console.log(colorAtt)
+            //colorChanger.removeAttribute('color-changer');
+            colorChanger.setAttribute('color-changer', colorAtt);  
+                
+        }
+        if(loader){
+            document.querySelector('a-scene').addEventListener('loaded', function () {
+                if(params.scene){
+                    loader.setAttribute('loader', '/src/scenes/'+params.scene+'.json');
+                    console.log(params.scene);
+                }
+            })
+            
+        }
+
+        
         for(i in pieces){
             pieces[i].object3D.position.x -=0.25;   // removes offset. Currently pieces are spaced throughout the range (x,z) (2,2) to (-1.5,-1.5)
             pieces[i].object3D.position.x *=0.16;   //Shrinks spacing between pieces.
@@ -15,6 +54,22 @@ AFRAME.registerComponent('setatts',{
             
         }
         
-    }
+         
+    },
+    getUrlParams: function () {//Copied from dynamic-room-component, itself from networked a-frame.
+        var match;
+        var pl = /\+/g;  // Regex for replacing addition symbol with a space
+        var search = /([^&=]+)=?([^&]*)/g;
+        var decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); };
+        var query = window.location.search.substring(1);
+        var urlParams = {};
+    
+        match = search.exec(query);
+        while (match) {
+          urlParams[decode(match[1])] = decode(match[2]);
+          match = search.exec(query);
+        }
+        return urlParams;
+      }
     
 });
