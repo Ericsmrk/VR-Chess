8750fb88af0603de5287a2580348e2f938483417
millerjacob49 <miller_jacob49@outlook.com>
2022-11-17 19:49:13 -0800

    Working on turns.

--- examples/js/repo_component.js
+++ examples/js/repo_component.js
@@ -1,26 +1,29 @@
-AFRAME.registerComponent('repo_b', {
+AFRAME.registerComponent('repo', {
 
     init: function() {
         const avatar1 = document.querySelector('#player')
-
-        function setPosb(){
-            avatar1.setAttribute('position', "0 1.2 -0.4")
-        }
-
-        this.el.addEventListener('click', setPosb);
-    }
-});
-
-
-AFRAME.registerComponent('repo_w', {
-
-    init: function() {
-        const avatar1 = document.querySelector('#player')
-
-        function setPos(){
-            avatar1.setAttribute('position', "0 1.2 0.4")
-        }
+        const sText = document.querySelector('#startText')
+        const pCursor = document.querySelector('#playercursor') 
+        const emp = document.querySelector('#empty');  
         
-        this.el.addEventListener('click', setPos);
+        this.el.addEventListener('click', function() {
+            if (this.getAttribute('id')=="whiteBox") {
+                avatar1.setAttribute('position', "0 1.2 0.4")
+                sText.setAttribute('visible', "false")
+                pCursor.setAttribute('playerID', "0")
+            }
+            else if (this.getAttribute('id')=="blackBox") {
+                    avatar1.setAttribute('position', "0 1.2 -0.4")
+                    sText.setAttribute('visible', "false")
+                    pCursor.setAttribute('playerID', "1")
+                }
+        })
     }
 });
+
+AFRAME.registerComponent('char', {
+    schema:{
+        playerID: {type: 'string', default: '0'}
+    }
+});
+
