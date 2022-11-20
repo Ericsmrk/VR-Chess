6c0f4c531c8dfd8bcf320f08b019e8e39de83425
millerjacob49 <miller_jacob49@outlook.com>
2022-11-19 20:15:08 -0800

    Fixed what I broke(holdable)

--- examples/js/repo_component.js
+++ examples/js/repo_component.js
@@ -3,20 +3,24 @@ AFRAME.registerComponent('repo', {
     init: function() {
         const avatar1 = document.querySelector('#player')
         const sText = document.querySelector('#startText')
-        const pCursor = document.querySelector('#playercursor') 
-        const emp = document.querySelector('#empty');  
+        const pCursor = document.querySelector('#playercursor')
+        
         
         this.el.addEventListener('click', function() {  //reset position, remove instructions, set playerID for turns
             if (this.getAttribute('id')=="whiteBox") {
                 avatar1.setAttribute('position', "0 1.2 0.4")
                 sText.setAttribute('visible', "false")
                 pCursor.setAttribute('playerID', "0")
+                this.setAttribute('text', "value:Player in session, turn around and select player 2")
             }
             else if (this.getAttribute('id')=="blackBox") {   //reset position, remove instructions, set playerID for turns
                     avatar1.setAttribute('position', "0 1.2 -0.4")
                     sText.setAttribute('visible', "false")
                     pCursor.setAttribute('playerID', "1")
+                    this.setAttribute('text', "value:Player in session, turn around and select player 1")
                 }
+
+            this.removeAttribute('geometry')
         })
     }
 });
@@ -31,5 +35,7 @@ AFRAME.registerComponent('turns', {
     schema:{
         whoseTurn: {type: 'string', default:"0"}
     }
-})
+});
+
+
 
