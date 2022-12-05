a506fcbee2ce997ebed32498a358a46e11d0190a
Christian Leon <christian2leon@gmail.com>
2022-12-05 12:20:27 -0800

    polishing

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+
+
 router.get('/regis', function (req, res, next) {
 	return res.render('index.ejs');
 });
